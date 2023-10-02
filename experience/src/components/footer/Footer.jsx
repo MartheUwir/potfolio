4eade17138e5a9134ab9e7f8f5d4618b5c2796d3
MartheUwir <marthuwir@gmail.com>
2023-10-02 08:13:05 +0200

    contact

--- experience/src/components/footer/Footer.jsx
+++ experience/src/components/footer/Footer.jsx
@@ -5,7 +5,20 @@ const Footer = () => {
     
     <footer>
       <a href="#" className='footer_logo'>Marthe</a>
-      
+      <li> <a href="home">home</a></li>
+      <li> <a href="about">about</a></li>
+      <li> <a href="experience">experience</a></li>
+      <li> <a href="services">services</a></li>
+      <li> <a href="portfolio">portfolio</a></li>
+      <li> <a href="testimonials">testimonials</a></li>
+      <li> <a href="contact">contact</a></li>
+ 
+    <div className="footer_socials">
+      <a href="https:facebook.com"></a>
+      <a href="https:facebook.com"></a>
+      <a href="https:facebook.com"></a>
+      <a href="https:facebook.com"></a>
+    </div>
     </footer>
   )
 }
